5ac6b25a92006eb8f3c952b9cb95858e8c2b2ffd
personaluser216-star <personaluser216@gmail.com>
2025-10-03 16:49:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ const AdminRouter = require("./Router/AdminRouter");
 const cors=require("cors");
 const ProductRouter = require("./Router/ProductRouter");
 const CartRouter = require("./Router/CartRouter");
+const OrderRouter = require("./Router/OrderRouter");
 
 const app=express();
-Connect()
+
 dotenv.config();
 const PORT = process.env.PORT || 4000
 app.use(express.json());
+Connect()
 app.use(cors({
   origin: "*",  
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
+
 app.use("/admin",AdminRouter);
 app.use("/product",ProductRouter);
 app.use("/cart",CartRouter);
+app.use("/order",OrderRouter);
+
 
 app.get("/",(req,res)=>
 {
